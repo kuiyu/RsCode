1d50b0d79a6464e82476d739a58f012ab4405c57
Tony <79599287@qq.com>
2021-01-12 15:59:40 +0800

    dele test

--- tests/RsCode.Storage.Tests/ObjectTest.cs
+++ tests/RsCode.Storage.Tests/ObjectTest.cs
@@ -68,21 +68,36 @@ namespace RsCode.Storage.Tests
         {
             string key1 = "1197/wxSop/59420fc80a95a1f70f289afa9aea21c0.jpg";
             string key2 = "1698/wxSop/0300602655adcfc96799c0e1d5e9d907.jpg";
-            string bucket = "rsyunpan";
-            List<QiniuStorageRequest> ops = new List<QiniuStorageRequest>(); 
- 
-            var url = new StatRequest(bucket, key2).GetApiUrl();
-            var s = url.Substring(url.IndexOf('/'));
-            var url2 = new StatRequest(bucket, key1).GetApiUrl();
-            var s2 = url2.Substring(url2.IndexOf('/'));
+            string bucket = "ttj-test";
+            List<QiniuStorageRequest> ops = new List<QiniuStorageRequest>();
 
-            //ops.Add(new StatRequest(bucket, key1));
-            //ops.Add(new StatRequest(bucket, key2)); 
+            ops.Add(new StatRequest(bucket, key1));
+            ops.Add(new StatRequest(bucket, key2));
+            var (ret, result) = await qiniu.SendAsync(new BatchRequest(bucket, ops));
 
-            ops.Add(new StatRequest(bucket, "美图/9"));
+        }
 
-            var (ret,result) = await qiniu.SendAsync(new BatchRequest(bucket,ops)); 
-            
+        [Fact]
+        public async Task DeleteTest()
+        {
+            string bucket = "test";
+            var key = "FkMQ7qBGT1OvjfdZNdbnzkIeJNyh";
+            var ret = await qiniu.SendAsync<DeleteResponse>(new DeleteRequest(bucket, key));
+            Assert.Equal(200, ret.HttpCode);
+        }
+
+        [Fact]
+        public async Task BatchDelete()
+        {
+            string bucket = "test";
+
+            List<QiniuStorageRequest> ops = new List<QiniuStorageRequest>();
+
+            ops.Add(new DeleteRequest(bucket, "FiOlnpxydS3WcZ3C2fXtRBe4tZoC"));
+            ops.Add(new DeleteRequest(bucket, "FijvYxiJCz1-oe3QpTnbXSYynU1V"));
+            var (ret, result) = await qiniu.SendAsync(new BatchRequest(bucket, ops));
+
+            Assert.Equal(200, (int)ret.StatusCode);
         }
     }
 }
