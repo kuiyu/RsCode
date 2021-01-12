ad9962951df761d96c8811e83669f9ab98285fd1
Tony <79599287@qq.com>
2021-01-12 21:05:12 +0800

    update

---
+++ tests/RsCode.AspNetCore.Tests/AutoMapperTest.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace RsCode.AspNetCore.Tests
+{
+    public class AutoMapperTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var a = new TestA();
+            var b=a.MapTo<TestB>();
+            Assert.Equal(a.Name, b.Name);
+
+            var c = a.MapTo<TestC>();
+            Assert.NotEqual(a.Name, c.Name);
+
+            List<TestA> aa = new List<TestA>();
+            aa.Add(a);
+            var cc = aa.MapToList<TestC>();
+            Assert.Equal(cc.FirstOrDefault().Nick, aa.FirstOrDefault().Nick);
+        }
+    }
+
+    public class TestA
+    {
+        public string Name { get; set; } = "rscode.cn";
+
+        public string Nick { get; set; } = "https://rescode.cn";
+
+       
+         
+
+    
+    }
+
+    public class TestB
+    {
+        public string Name { get; set; }
+
+        public string Nick { get; set; }
+    }
+    public class TestC
+    {
+        [AutoMapper.IgnoreMap]
+        public string Name { get; set; } = "rs888.net";
+        
+        public string Nick { get; set; }
+        [JsonPropertyName("isEdit")]
+        public bool Say { get; set; }
+
+    }
+}
